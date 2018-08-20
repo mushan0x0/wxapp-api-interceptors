59a0806652c3d2c99b2467cf9545ca8fe20a891f
吴俊骁 <mushan0x0@gmail.com>
2018-08-20 09:31:29 +0800

    使用插件

--- example/wxapp/page/example.js
+++ example/wxapp/page/example.js
@@ -1,17 +1,20 @@
 var regeneratorRuntime = require('../runtime'); // 需要用到async的地方都要引入这个文件，同时需要打开es6转es5
 
 Page({
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  async onLoad(options) {
-      await wx.showModal({title: '测试'})
-          .then(console.log)
-          .finally(() => {
-              console.log('finally')
-          });
-      console.log('结束');
-      console.log(wx.createAnimation());
-      wx.checkSession().then(console.log);
-  },
+    data: {
+        richText: '<h1>Hello world!</h1>'
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    async onLoad(options) {
+        await wx.showModal({title: '测试'})
+            .then(console.log)
+            .finally(() => {
+                console.log('finally')
+            });
+        console.log('结束');
+        console.log(wx.createAnimation());
+        wx.checkSession().then(console.log);
+    },
 });
